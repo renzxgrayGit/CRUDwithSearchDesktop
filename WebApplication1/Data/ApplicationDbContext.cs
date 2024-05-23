4f3af7b87e7a2f9c8623b28ad0d75e0f885d5179
renzxgrayGit <renzocabajar9@gmail.com>
2024-05-23 11:04:11 +0800

    Add project files.

---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models.Entities;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
